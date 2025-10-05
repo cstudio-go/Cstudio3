6d3b1b9f2fddd764b88ede66637b5e97cd0bf5b8
tuvibo <tuvibo@gmail.com>
2025-10-06 06:51:10 +0800

    5th commmit

---
+++ splash.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Wait for splash screen animation to finish (3 seconds)
+    setTimeout(() => {
+    window.location.href = "main.html"; // Replace 'main.html' with your target HTML file
+}, 4400); // Match the animation duration in CSS
+});
+  
