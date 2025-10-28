c476c9b5b446bf51be31cfed5f571d3ed547875f
tuvibo <tuvibo@gmail.com>
2025-10-28 15:43:09 +0800

    a17

--- chat/db_config.php
+++ chat/db_config.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "chatdb";
+$username = "cstusybk_cstudio";
+$password = "Aaron176!!!!@@@@";
+$dbname = "cstusybk_chatdb";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
