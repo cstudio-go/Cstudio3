18a706de3860021a4bd4a8465f86fada0ea195dd
tuvibo <tuvibo@gmail.com>
2025-11-09 00:29:10 +0800

    a89

---
+++ eshop/products.php
@@ -0,0 +1,22 @@
+<?php include('includes/db_connect.php'); ?>
+<!DOCTYPE html>
+<html>
+<head><title>Products</title></head>
+<body>
+<h1>Products</h1>
+
+<?php
+$sql = "SELECT * FROM products";
+$result = $conn->query($sql);
+
+while ($row = $result->fetch_assoc()) {
+  echo "<div>";
+  echo "<img src='images/" . $row['image'] . "' width='150'>";
+  echo "<h2>" . $row['name'] . "</h2>";
+  echo "<p>$" . $row['price'] . "</p>";
+  echo "<a href='product.php?id=" . $row['id'] . "'>View</a>";
+  echo "</div>";
+}
+?>
+</body>
+</html>
