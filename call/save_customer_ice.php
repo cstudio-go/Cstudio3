c91f96113c79762c9a806d7469220d7b3e9245de
tuvibo <tuvibo@gmail.com>
2025-11-17 05:48:48 +0800

    b19

--- call/save_customer_ice.php
+++
@@ -1,2 +0,0 @@
-<?php
-file_put_contents("signal/customer_ice.json", file_get_contents("php://input"));
--- call/save_offer.php
+++
@@ -1,2 +0,0 @@
-<?php
-file_put_contents("signal/offer.json", file_get_contents("php://input"));
