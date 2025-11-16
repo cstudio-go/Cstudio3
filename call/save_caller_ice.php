99f72c7494f3c453936aef302cc1a98b0087f2ab
tuvibo <tuvibo@gmail.com>
2025-11-17 02:23:28 +0800

    b15

---
+++ call/save_caller_ice.php
@@ -0,0 +1,2 @@
+<?php
+file_put_contents("signal/caller_ice.json", file_get_contents("php://input"));
---
+++ call/save_offer.php
@@ -0,0 +1,2 @@
+<?php
+file_put_contents("signal/offer.json", file_get_contents("php://input"));
